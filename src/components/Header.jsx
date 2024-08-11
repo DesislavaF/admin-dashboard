8b73cb57ad3cc454afc5b6ff58e7d455cf5e5395
DesislavaF <156531205+DesislavaF@users.noreply.github.com>
2024-08-11 21:54:17 +0300

    commit SerachBar and Navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './header.css';
 import Logo from './Logo';
+import SearchBar from './SearchBar';
+import Nav from './Nav';
 
 function Header() {
   return (
     <header 
     id='header' className='header fixes-top d-flex align-items-center'>
       <Logo />
+      <SearchBar />
+      <Nav />
     </header>
   );
 }
